957de602847bc3dc7fe989924fd32c1eec73ef7a
Adam Chmara <achmara@frinx.io>
2020-11-18 15:25:35 +0100

    Set which tasks are disabled by default

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from 'frinx-workflow-ui/lib/App'
 const conductorApiUrlPrefix = "/uniflow/api/conductor";
 const frontendUrlPrefix = "/uniflow/ui";
 const enableScheduling = true;
-const disabledTasks = ["js", "py", "graphQL"];
+const disabledTasks = ["js", "py", "graphQL", "while", "while_end"];
 const prefixHttpTask = '';
 
 ReactDOM.render(
